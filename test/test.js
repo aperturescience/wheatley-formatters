77e32f07f16a9e6ecd57997ca42b0095685fafd7
Gilles De Mey <gilles.de.mey@gmail.com>
2014-03-02 16:46:07 -0800

    unit tests for bytes formatter

---
+++ test/test.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var assert = require('assert'),
+    format = require('../format');
+
+describe('format', function () {
+
+  // terabytes
+  describe('#byte(2199023255552)', function () {
+    it('should return 2.00 TB', function () {
+      var s = format.bytes(2199023255552);
+      var a = '2.00 TB';
+      assert.equal(s, a);
+    });
+  });
+
+  // gigabytes
+  describe('#byte(8589934592)', function () {
+    it('should return 8.00GB', function () {
+      var s = format.bytes(8589934592);
+      var a = '8.00 GB';
+      assert.equal(s, a);
+    });
+  });
+
+  // megabytes
+  describe('#byte(536870912)', function () {
+    it('should return 512.00 MB', function () {
+      var s = format.bytes(536870912);
+      var a = '512.00 MB';
+      assert.equal(s, a);
+    });
+  });
+
+
+  // kilobytes
+  describe('#byte(4096)', function () {
+    it('should return 4.00 kB', function () {
+      var s = format.bytes(4096);
+      var a = '4.00 kB';
+      assert.equal(s, a);
+    });
+  });
+
+  // bytes
+  describe('#byte(4)', function () {
+    it('should return 4.00 bytes', function () {
+      var s = format.bytes(4);
+      var a = '4.00 bytes';
+      assert.equal(s, a);
+    });
+  });
+
+});
